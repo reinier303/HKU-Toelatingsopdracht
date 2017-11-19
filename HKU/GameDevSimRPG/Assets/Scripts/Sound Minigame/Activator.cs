3381516bd83e78ad04f38d4b1744bc9411125a20
reinier303 <30725987+reinier303@users.noreply.github.com>
2017-11-19 23:02:56 +0100

    Add project

---
+++ HKU/GameDevSimRPG/Assets/Scripts/Sound Minigame/Activator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Activator : MonoBehaviour {
+
+    public KeyCode key;
+
+    bool active = false;
+
+    GameObject note;
+
+    public Image image;
+
+    public Sprite baseSprite;
+    public Sprite downSprite;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            image.sprite = downSprite;
+        }
+        if (Input.GetKeyUp(key))
+        {
+            image.sprite = baseSprite;
+        }
+        if (Input.GetKeyDown(key) && active)
+        {
+            Destroy(note);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active = true;
+
+        if(col.gameObject.tag == "Note")
+        {
+            note = col.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        active = false;
+    }
+
+
+}
