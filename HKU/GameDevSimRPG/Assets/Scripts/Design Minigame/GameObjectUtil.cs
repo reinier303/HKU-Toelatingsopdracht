440ac72d1a9a9e57a1fc9ba4cfb1a7b12aa660fa
reinier303 <30725987+reinier303@users.noreply.github.com>
2017-12-28 18:42:38 +0100

    Object pool complete

--- HKU/GameDevSimRPG/Assets/Scripts/Design Minigame/GameObjectUtil.cs
+++ HKU/GameDevSimRPG/Assets/Scripts/Design Minigame/GameObjectUtil.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class GameObjectUtil
 {
+    private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>();
+
     public static GameObject Instantiate(GameObject prefab, Vector3 pos)
     {
         GameObject instance = null;
 
-        instance = GameObject.Instantiate(prefab);
-        instance.transform.position = pos;
+        var recycledScript = prefab.GetComponent<RecycleGameObject>();
+        if (recycledScript != null)
+        {
+            var pool = GetObjectPool(recycledScript);
+            instance = pool.NextObject(pos).gameObject;
+        }
+        else
+        {
+            instance = GameObject.Instantiate(prefab);
+            instance.transform.position = pos;
+        }
 
         return instance;
     }
@@ -29,5 +40,23 @@ public class GameObjectUtil
 
     }
 
+    private static ObjectPool GetObjectPool(RecycleGameObject reference)
+    {
+        ObjectPool pool = null;
+
+        if(pools.ContainsKey(reference))
+        {
+            pool = pools[reference];
+        }
+        else
+        {
+            var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
+            pool = poolContainer.AddComponent<ObjectPool>();
+            pool.prefab = reference;
+            pools.Add(reference, pool);
+        }
+
+        return pool;
+    }
 
 }
