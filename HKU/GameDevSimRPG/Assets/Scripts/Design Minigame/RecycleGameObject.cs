24395ecfa7d4194254d2c861d02498e029b2607a
reinier303 <30725987+reinier303@users.noreply.github.com>
2017-12-28 18:53:07 +0100

    objectpool 1.1

--- HKU/GameDevSimRPG/Assets/Scripts/Design Minigame/RecycleGameObject.cs
+++ HKU/GameDevSimRPG/Assets/Scripts/Design Minigame/RecycleGameObject.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IRecycle
+{
+    void Restart();
+    void Shutdown();
+}
+
 public class RecycleGameObject : MonoBehaviour {
+
+    private List<IRecycle> recycleComponents;
+
+    void Awake()
+    {
+        var components = GetComponents<MonoBehaviour>();
+        recycleComponents = new List<IRecycle>();
+        foreach(var component in components)
+        {
+            if(component is IRecycle)
+            {
+                recycleComponents.Add(component as IRecycle);
+            }
+        }
+
+        Debug.Log(name + " Found" + recycleComponents.Count + " Components");
+    }
 
     public void Restart()
     {
         gameObject.SetActive(true);
+
+        foreach(var component in recycleComponents)
+        {
+            component.Restart();
+        }
     }
 
     public void ShutDown()
     {
         gameObject.SetActive(false);
+
+        foreach (var component in recycleComponents)
+        {
+            component.Shutdown();
+        }
     }
 }
