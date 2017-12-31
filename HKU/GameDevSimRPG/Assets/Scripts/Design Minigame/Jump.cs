4c96af9a4f5827a2b41d40cb78d2a8001bc1d670
reinier303 <30725987+reinier303@users.noreply.github.com>
2017-12-31 18:28:02 +0100

    better stones and begin jumping

---
+++ HKU/GameDevSimRPG/Assets/Scripts/Design Minigame/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+    public float jumpSpeed = 240;
+    
+    private Rigidbody2D body2d;
+    private InputState inputState;
+
+	// Use this for initialization
+	void Awake ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
