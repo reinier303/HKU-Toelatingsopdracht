52c3416a50b7fb2d72d47f797d70447fdce3b37f
reinier303 <30725987+reinier303@users.noreply.github.com>
2018-01-02 02:48:50 +0100

    maingame start

--- HKU/GameDevSimRPG/Assets/Scripts/Main Game/SkillManager.cs
+++ HKU/GameDevSimRPG/Assets/Scripts/Main Game/SkillManager.cs
@@ -5,27 +5,62 @@ using UnityEngine.UI;
 
 public class SkillManager : MonoBehaviour {
 
+    public float designLvl;
+    public float artLvl;
+    public float soundLvl;
+    public float codingLvl;
+
     public float designExp;
     public float artExp;
     public float soundExp;
-    public float CodingExp;
+    public float codingExp;
 
     public float designExpNeeded = 250;
     public float artExpNeeded = 250;
     public float soundExpNeeded = 250;
-    public float CodingExpNeeded = 250;
+    public float codingExpNeeded = 250;
 
     public Slider designBar;
     public Slider artBar;
     public Slider soundBar;
     public Slider codingBar;
-	
-	// Update is called once per frame
-	void Update () {
+
+    public Text designLvlText;
+    public Text artLvlText;
+    public Text soundLvlText;
+    public Text codingLvlText;
+
+    // Update is called once per frame
+    void Update () {
         designBar.value = designExp / designExpNeeded;
         artBar.value = artExp / artExpNeeded;
         soundBar.value = soundExp / soundExpNeeded;
-        codingBar.value = CodingExp / CodingExpNeeded;
+        codingBar.value = codingExp / codingExpNeeded;
 
+        designLvlText.text = "Lvl: " + designLvl;
+        artLvlText.text = "Lvl: " + artLvl;
+        soundLvlText.text = "Lvl: " + soundLvl;
+        codingLvlText.text = "Lvl: " + codingLvl;
+
+        if (designExp >= designExpNeeded)
+        {
+            designLvl++;
+            designExpNeeded *= Mathf.Round(1.5f * designExpNeeded);
+        }
+        if (artExp >= artExpNeeded)
+        {
+            artLvl++;
+            designExpNeeded *= Mathf.Round(1.5f * designExpNeeded);
+        }
+        if (soundExp >= soundExpNeeded)
+        {
+            soundLvl++;
+            designExpNeeded *= Mathf.Round(1.5f * designExpNeeded);
+        }
+        if (codingExp >= codingExpNeeded)
+        {
+            codingLvl++;
+            designExpNeeded *= Mathf.Round(1.5f * designExpNeeded);
+        }
     }
 }
