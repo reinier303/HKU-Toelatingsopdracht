9a6a83ee4507efe86ad0df38e8a44f02cebdf9da
reinier303 <reinier@warp.industries>
2018-01-19 17:01:50 +0100

    addded blinking continue

---
+++ HKU/GameDevSimRPG/Assets/Scripts/StartScreen/BlinkingCursor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingCursor : MonoBehaviour {
+
+    public Text BlinkField;
+    public Text inputField;
+    public int maxCharacters = 30;
+
+    private float delay = 0;
+    private float cursorDelay = 0.65f;
+    private bool blink;
+    private string _text = "";
+
+    // Update is called once per frame
+    void Update () {
+        var text = _text;
+
+        if(inputField.text.Length < maxCharacters)
+        {
+            text += "Click to Continue";
+
+            if(blink)
+            {
+                text = "";
+            }
+        }
+
+        BlinkField.text = text;
+
+        delay += Time.deltaTime;
+        if(delay > cursorDelay)
+        {
+            delay = 0;
+            blink = !blink;
+        }
+	}
+}
