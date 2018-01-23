5b0890acd25bb174732d11906be9e812a407dbad
reinier303 <30725987+reinier303@users.noreply.github.com>
2018-01-23 15:14:04 +0100

    Menu

---
+++ HKU/GameDevSimRPG/Assets/Scripts/Main Game/MenuOn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOn : MonoBehaviour {
+
+    public GameObject MenuPanel;
+
+    public void OpenMenu()
+    {
+        if(!MenuPanel.activeSelf)
+        {
+            MenuPanel.SetActive(true);
+        }
+        else
+        {
+            MenuPanel.SetActive(false);
+        }
+    }
+}
