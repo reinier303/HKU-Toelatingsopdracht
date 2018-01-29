df99008a126da7928c76f0fb7f94b0fc8dcaadc2
reinier303 <reinier@warp.industries>
2018-01-29 11:32:08 +0100

    savestats

---
+++ HKU/GameDevSimRPG/Assets/Scripts/Main Game/SaveStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveStats : MonoBehaviour {
+
+    public static void StatSave()
+    {
+        //Lvls
+        PlayerPrefs.SetInt("CompanyLvl", );
+        PlayerPrefs.SetInt("DesignLvl", );
+        PlayerPrefs.SetInt("CodingLvl", );
+        PlayerPrefs.SetInt("ArtLvl", );
+        PlayerPrefs.SetInt("SoundLvl", );
+
+        //Money and Fans
+        PlayerPrefs.SetFloat("Money",);
+        PlayerPrefs.SetFloat("Fans",);
+
+        //Exp
+        PlayerPrefs.SetFloat("CompanyExp", );
+        PlayerPrefs.SetFloat();
+        PlayerPrefs.SetFloat();
+        PlayerPrefs.SetFloat();
+        PlayerPrefs.SetFloat();
+    }
+}
